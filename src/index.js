42c48b5c7d23f4265c7821737e007c483541f27d
DNX <auraltension@riseup.net>
2019-05-23 09:13:24 +1000

    Refactor Grid to move function

--- src/index.js
+++ src/index.js
@@ -2,18 +2,23 @@ export const sumOfTwoInteger = (x, y) => x + y;
 
 export const diffOfTwoInteger = (x, y) => x - y;
 
+function makeGrid(x, y){
+  const grid = [];
+  for (let i = 0; i < x; i++){
+    const r = []
+    for (let j = 0; j < y; j++){
+      r.push(false);
+    }
+    grid.push(r);
+  }
+  return grid;
+}
+
 export class Grid {
   constructor(x, y){
     this.x = x;
     this.y = y;
-    this.grid = []
-    for (let i = 0; i < x; i++){
-      const r = []
-      for (let j = 0; j < y; j++){
-        r.push(false);
-      }
-      this.grid.push(r);
-    }
+    this.grid = makeGrid(x, y)
   }
   get(x, y){
     return this.grid[x][y];
