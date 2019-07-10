baa282208521cfa151cf2b51c75cb1f90bea63c4
DNX <auraltension@riseup.net>
2019-07-11 07:33:04 +1000

    indentation consistency

--- src/index.js
+++ src/index.js
@@ -24,16 +24,16 @@ function assertIsBoolean(b){
 }
 
 function between(min, max, value) {
-      return (min < value) && (value < max);
+  return (min < value) && (value < max);
 }
 
 function decideFate(currState, neighbourStates) {
-    /*
-    1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-    2. Any live cell with two or three live neighbours lives on to the next generation.
-    3. Any live cell with more than three live neighbours dies, as if by overpopulation.
-    4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.  
-    */
+  /*
+  1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+  2. Any live cell with two or three live neighbours lives on to the next generation.
+  3. Any live cell with more than three live neighbours dies, as if by overpopulation.
+  4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.  
+  */
   if (currState) {
     if (neighbourStates.live < 2) {
       return false
